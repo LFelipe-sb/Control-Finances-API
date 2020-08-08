fc2071d77b897d697d577a95dd2d4d02c726855d
Luis Felipe <luisfelipe.sb@outlook.com>
2020-08-08 20:43:38 -0300

    Tratamento de erros, controle de logs e compartilhamento

--- Routes/finances.js
+++ Routes/finances.js
@@ -4,38 +4,67 @@ import {insertFinances, totalMonth} from '../controllers/financesController.js';
 
 const router = express.Router();
 
-router.get('/', async(req, res) => {
-    const dataFile = JSON.parse( await fs.readFile('finances.json'));
-    delete dataFile.nextId;
-    res.send(dataFile);
+router.get('/', async(_req, res, next) => {
+    try{
+        const dataFile = JSON.parse( await fs.readFile('finances.json'));
+        delete dataFile.nextId;
+        res.send(dataFile);
+    } catch(err) {
+        next(err);
+    }
 });
 
 router.get('/:id', async(req, res) => {
-    const dataFile = JSON.parse( await fs.readFile('finances.json'));
-    const index = dataFile.finances.find((register) => register.id === parseInt(req.params.id));
-    res.send(index);
+    try{
+        const dataFile = JSON.parse( await fs.readFile('finances.json'));
+        const index = dataFile.finances.find((register) => register.id === parseInt(req.params.id));
+        res.send(index);
+    } catch(err) {
+        next(err);
+    }
 });
 
 router.get('/totalMonth/:month', async(req, res) => {
-    res.send( await totalMonth(parseInt(req.params.month)));
+    try{
+        res.send( await totalMonth(parseInt(req.params.month)));
+    } catch(err) {
+        next(err);
+    }
 });
 
 router.post('/balance', async(req, res) => {
-    const {value, category, date} = req.body;
-    res.send( await insertFinances(Math.abs(value), category, date));
+    try{
+        const {value, category, date} = req.body;
+        res.send( await insertFinances(Math.abs(value), category, date));
+    } catch(err){
+        next(err);
+    }
 });
 
 router.post('/expenses', async(req, res) => {
-    const {value, category, date} = req.body;
-    res.send( await insertFinances(Math.abs(value), category, date, 'D'));
+    try{
+        const {value, category, date} = req.body;
+        res.send( await insertFinances(Math.abs(value), category, date, 'D'));
+    } catch(err) {
+        next(err);
+    }
 });
 
 router.delete('/:id', async(req, res) => {
-    const dataFile = JSON.parse( await fs.readFile('finances.json'));
-    dataFile.finances = dataFile.finances.filter((register) => register.id !== parseInt(req.params.id));
+    try{
+        const dataFile = JSON.parse( await fs.readFile('finances.json'));
+        dataFile.finances = dataFile.finances.filter((register) => register.id !== parseInt(req.params.id));
 
-    await fs.writeFile('finances.json', JSON.stringify(dataFile, null, 2));
-    res.send('Item successfully deleted.');
+        await fs.writeFile('finances.json', JSON.stringify(dataFile, null, 2));
+        res.send('Item successfully deleted.');
+    } catch(err) {
+        next(err);
+    }
+});
+
+router.use((err, req, res, _next) => {
+    global.logger.error(`${req.method} ${req.baseUrl} -> ${err.message}`)
+    res.status(500).send('An error has occurred. ' + err);
 });
 
 export default router;
--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 import express from 'express';
 import financesRouter from './Routes/finances.js'
 import { promises as fs} from 'fs';
+import cors from 'cors';
+import winston from 'winston';
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/finances', financesRouter);
+
+const {combine, label, timestamp, printf} = winston.format;
+const myFormat = printf(({level, message, label, timestamp}) => {
+    return `${timestamp} [${label}] -> ${level}: ${message}`;
+});
+
+global.logger = winston.createLogger({
+    level: "silly",
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({filename: "finances-control-api.log"})
+    ],
+    format: combine(
+        label({label: "finances-control"}),
+        timestamp(),
+        myFormat
+    )
+});
 
 app.listen(3001, async() => {
     try {
         await fs.readFile('finances.json');
-        console.log('API Started.');
+        global.logger.info('API Started.');
     } catch (error) {
         const initialJson = {
             nextId: 1,
             finances: [],
         }
         await fs.writeFile('finances.json', JSON.stringify(initialJson, null, 2));
-        console.log('API Started and File created.');
+        global.logger.info('API Started and File created.');
     }
 });
